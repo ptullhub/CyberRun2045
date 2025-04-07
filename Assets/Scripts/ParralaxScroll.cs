4f710c380ae4de9a31b199359d5d7cd401aa143d
ptullhub <111908049+ptullhub@users.noreply.github.com>
2025-04-07 06:07:14 -0400

    parralaxscroll

---
+++ Assets/Scripts/ParralaxScroll.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class ParallaxLayer : MonoBehaviour
+{
+    [SerializeField] private float scrollSpeed = 0.1f;
+    [SerializeField] private Renderer layerRenderer;
+
+    private float offset;
+    private bool isScrolling = true;
+
+    private void Start()
+    {
+        GameManager.GameInstance.onGameOver.AddListener(StopScrolling);
+    }
+    void Update()
+    {
+        if (!isScrolling) return;
+
+        offset += Time.deltaTime * scrollSpeed;
+        layerRenderer.material.mainTextureOffset = new Vector2(offset, 0f);
+    }
+
+    public void StopScrolling()
+    {
+        isScrolling = false;
+    }
+}
